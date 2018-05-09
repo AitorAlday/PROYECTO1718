ee427ca1cc117b85bfc104b5e070c0fb89839b58
Unknown <aitoraldava@gmail.com>
2018-05-09 11:23:34 +0200

    Auto stash before merge of "master" and "origin/master"

--- BASES/Procedures.sql
+++ BASES/Procedures.sql
@@ -1,1 +1,12 @@
 --PROCEDURE GENERAR USUARIO Y LA CONTRASEÑA
+CREATE OR REPLACE PROCEDURE PROCE_generarAutoUserPass(v_dni IN VARCHAR , v_nombre IN VARCHAR, v_apellido IN VARCHAR, v_tipo IN VARCHAR, v_codlogin OUT INTEGER) AS
+  userr VARCHAR(30);
+  pass VARCHAR(30);
+BEGIN 
+  userr := LOWER(v_dni);
+  pass := LOWER(substr(v_nombre,1,1) || v_apellido);
+   INSERT INTO login (usuario, contraseña, tipo) VALUES (usuario, contrasenia, v_tipo) RETURNING codlogin INTO v_codlogin;
+EXCEPTION 
+  WHEN OTHERS THEN 
+    RAISE_APPLICATION_ERROR (-20100, 'ERROR INEXPERADO AL GENERAR EL LOGIN - USUARIO/CONTRASENIA: '|| SQLERRM);
+END;
--- BASES/Tablas.sql
+++ BASES/Tablas.sql
@@ -1,62 +1,110 @@
 --LAS TABLAS DEL PROYECTO
-
 DROP TABLE Equipo CASCADE CONSTRAINTS;
 CREATE TABLE Equipo (
-  id_equipo INTEGER NOT NULL,
-  nombre VARCHAR(15) NOT NULL,
-  
-  CONSTRAINT id_eq_pk PRIMARY KEY(id_equipo));
+  id_equipo INTEGER 
+    GENERATED ALWAYS AS IDENTITY
+                      MINVALUE 1
+                      MAXVALUE 999999
+                      INCREMENT BY 1
+                      START WITH 1
+                      NOORDER NOCYCLE NOT NULL
+                      ENABLE
+            CONSTRAINT id_eq_pk PRIMARY KEY,
+  nombre VARCHAR(15) NOT NULL 
+  );
 
 DROP TABLE Jugador CASCADE CONSTRAINTS;
 CREATE TABLE Jugador (
-  id_jugador INTEGER NOT NULL,
+  id_jugador INTEGER 
+    GENERATED ALWAYS AS IDENTITY
+                      MINVALUE 1
+                      MAXVALUE 999999
+                      INCREMENT BY 1
+                      START WITH 1
+                      NOORDER NOCYCLE NOT NULL
+                      ENABLE
+             CONSTRAINT id_ju_pk PRIMARY KEY,
   nickname VARCHAR(15) UNIQUE NOT NULL,
   nombre VARCHAR(15) NOT NULL,
   sueldo NUMBER(6,2) NOT NULL,
   id_equipo INTEGER NOT NULL,
   
-  CONSTRAINT id_ju_pk PRIMARY KEY(id_jugador),
   CONSTRAINT id_eqj_fk FOREIGN KEY(id_equipo) REFERENCES Equipo (id_equipo));
   
+
 DROP TABLE Persona CASCADE CONSTRAINTS;
 CREATE TABLE Persona (
-  id_persona INTEGER NOT NULL,
+  id_persona INTEGER 
+    GENERATED ALWAYS AS IDENTITY
+                      MINVALUE 1
+                      MAXVALUE 999999
+                      INCREMENT BY 1
+                      START WITH 1
+                      NOORDER NOCYCLE NOT NULL
+                      ENABLE
+             CONSTRAINT id_pe_pk PRIMARY KEY,
   nombre VARCHAR(15) NOT NULL,
-  usuario VARCHAR(15) NOT NULL,
-  contraseña VARCHAR(15) NOT NULL,
   tipo INTEGER NOT NULL,
-  id_equipo INTEGER NOT NULL,
+  id_equipo INTEGER NULL,
   
-  CONSTRAINT id_pe_pk PRIMARY KEY (id_persona),
   CONSTRAINT id_eqp_fk FOREIGN KEY (id_equipo) REFERENCES Equipo (id_equipo));
   
-DROP TABLE Jornada CASCADE CONSTRAINTS;  
-CREATE TABLE Jornada (
-  id_jornada INTEGER NOT NULL,
-  fec_ini DATE NOT NULL,
-  fec_fin DATE NOT NULL,
+DROP TABLE Login CASCADE CONSTRAINTS;
+CREATE TABLE Login (
+  id_login INTEGER 
+    GENERATED ALWAYS AS IDENTITY
+                      MINVALUE 1
+                      MAXVALUE 999999
+                      INCREMENT BY 1
+                      START WITH 1
+                      NOORDER NOCYCLE NOT NULL
+                      ENABLE
+           CONSTRAINT id_lo_pk PRIMARY KEY,
+  usuario VARCHAR(15) NOT NULL,
+  contraseña VARCHAR(15) NOT NULL,
+  id_persona INTEGER NOT NULL,
   
-  CONSTRAINT id_jo_pk PRIMARY KEY (id_jornada));
+  CONSTRAINT id_per_fk FOREIGN KEY (id_persona) REFERENCES Persona (id_persona));
+  
+  
+DROP TABLE Jornada CASCADE CONSTRAINTS;
+CREATE TABLE Jornada (
+  id_jornada INTEGER 
+    GENERATED ALWAYS AS IDENTITY
+                      MINVALUE 1
+                      MAXVALUE 999999
+                      INCREMENT BY 1
+                      START WITH 1
+                      NOORDER NOCYCLE NOT NULL
+                      ENABLE
+             CONSTRAINT id_jo_pk PRIMARY KEY,
+  fec_ini DATE NOT NULL,
+  fec_fin DATE NOT NULL
+);
   
 DROP TABLE Partido CASCADE CONSTRAINTS;
 CREATE TABLE Partido (
-  id_partido INTEGER NOT NULL,
+  id_partido INTEGER 
+    GENERATED ALWAYS AS IDENTITY
+                      MINVALUE 1
+                      MAXVALUE 999999
+                      INCREMENT BY 1
+                      START WITH 1
+                      NOORDER NOCYCLE NOT NULL
+                      ENABLE
+             CONSTRAINT id_pa_pk PRIMARY KEY,
   fecha DATE NOT NULL,
-  puntos_loc INTEGER NOT NULL,
-  puntos_vis INTEGER NOT NULL,
+  id_local INTEGER,
+  id_visitante INTEGER,
+  id_ganador INTEGER,
+  resultado VARCHAR(10),
   id_jornada INTEGER NOT NULL,
   
-  CONSTRAINT id_pa_pk PRIMARY KEY (id_partido),
-  CONSTRAINT id_jo_fk FOREIGN KEY (id_jornada) REFERENCES Jornada (id_jornada));
- 
-DROP TABLE Juegan CASCADE CONSTRAINTS; 
-CREATE TABLE Juegan (
-  id_partido INTEGER NOT NULL,
-  id_equipo INTEGER NOT NULL,
-  
-  CONSTRAINT id_pa_fk FOREIGN KEY (id_partido) REFERENCES Partido (id_partido),
-  CONSTRAINT id_eqju_fk FOREIGN KEY(id_equipo) REFERENCES Equipo (id_equipo)); 
-  
+  CONSTRAINT id_jo_fk FOREIGN KEY (id_jornada) REFERENCES Jornada (id_jornada),
+  CONSTRAINT id_local_fk FOREIGN KEY (id_local) REFERENCES Equipo (id_equipo),
+  CONSTRAINT id_visit_fk FOREIGN KEY (id_visitante) REFERENCES Equipo (id_equipo),
+  CONSTRAINT id_gana_fk FOREIGN KEY (id_ganador) REFERENCES Equipo (id_equipo)
+  );
   
   
   
